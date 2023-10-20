4e4990087d07d3073499cb878f3cd03af4238187
Morshed-17 <morshedmto@gmail.com>
2023-10-20 23:34:41 +0600

    update phone method added

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('hello world')
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-})
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.b2w59kw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,42 +28,61 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    const brandsCollection = client.db("phonesDB").collection('brands')
-    const phonesCollection = client.db("phonesDB").collection('phones')
-   app.get('/brands', async(req, res) => {
-        const cursor = brandsCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-   })
-   app.get('/phones/:brand', async(req, res) => {
-        const brand = req.params.brand
-        console.log(brand);
-        const query = {brand: brand}
-        const cursor =  phonesCollection.find(query)
-        const result = await cursor.toArray()
-        res.send(result)
-   })
-   app.get('/phones', async(req, res) => {
-    const cursor = phonesCollection.find()
-    const result = await cursor.toArray()
-    res.send(result)
-   })
-   app.post('/phones', async(req, res) => {
-        const newProduct = req.body
-        const result = await phonesCollection.insertOne(newProduct)
-        res.send(result)
-
-   })
-   app.get('/phone/:id', async(req, res) => {
-        const id = req.params.id
-        const query = {_id: new ObjectId(id)}
-        const result = await phonesCollection.findOne(query)
-        res.send(result)
-   })
-   
+    const brandsCollection = client.db("phonesDB").collection("brands");
+    const phonesCollection = client.db("phonesDB").collection("phones");
+    app.get("/brands", async (req, res) => {
+      const cursor = brandsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/phones/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      console.log(brand);
+      const query = { brand: brand };
+      const cursor = phonesCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/phones", async (req, res) => {
+      const cursor = phonesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.post("/phones", async (req, res) => {
+      const newProduct = req.body;
+      const result = await phonesCollection.insertOne(newProduct);
+      res.send(result);
+    });
+    app.get("/phone/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await phonesCollection.findOne(query);
+      res.send(result);
+    });
+    app.put("/phone/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const option = { upsert: true };
+      const updatedPhone = req.body;
+      const phone = {
+        $set: {
+          image: updatedPhone.image,
+          name: updatedPhone.name,
+          price: updatedPhone.price,
+          rating: updatedPhone.rating,
+          description: updatedPhone.description,
+          brand: updatedPhone.brand,
+          type: updatedPhone.type,
+        },
+      };
+      const result = await phonesCollection.updateOne(filter, phone, option);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -72,7 +90,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.listen(port,()=> {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
