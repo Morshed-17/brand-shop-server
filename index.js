2f925d1a23fb988b15e5c74317830ce7736e7df7
Morshed-17 <morshedmto@gmail.com>
2023-10-20 13:09:04 +0600

    phones and brands endpoint added

--- index.js
+++ index.js
@@ -2,5 +2,58 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 5000
 
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+
+})
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.b2w59kw.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const brandsCollection = client.db("phonesDB").collection('brands')
+    const phonesCollection = client.db("phonesDB").collection('phones')
+   app.get('/brands', async(req, res) => {
+        const cursor = brandsCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+   })
+   app.post('/phones', async(req, res) => {
+        const newProduct = req.body
+        const result = await phonesCollection.insertOne(newProduct)
+        res.send(result)
+      
+   })
+   
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port,()=> {
+    console.log(`Example app listening on port ${port}`)
+})
