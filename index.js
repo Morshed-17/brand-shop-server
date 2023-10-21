ad1b0c67ab18612e532a2179694b5567482a5fc9
Morshed-17 <morshedmto@gmail.com>
2023-10-21 14:43:26 +0600

    delete operation created for mycart

--- index.js
+++ index.js
@@ -89,6 +89,12 @@ async function run() {
       const result = await cursor.toArray()
       res.send(result)
 })
+app.delete('/mycart/:id', async(req, res) => {
+  const id = req.params.id
+  const query = {_id: new ObjectId(id)}
+  const result = await cartCollection.deleteOne(query)
+  res.send(result)
+})
 
     await client.db("admin").command({ ping: 1 });
     console.log(
